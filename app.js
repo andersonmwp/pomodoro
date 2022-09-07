b2966a1a17962325c4a2395578f103ce8c5f1e75
Anderson <andersonmwp@gmail.com>
2022-09-07 12:17:06 -0300

    correção do bug do timer e inclusão do stop ciclos

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const minutos = document.querySelector('.minutos');
 const segundos = document.querySelector('.segundos');
 const btn = document.querySelector('.btn');
+const ciclos = document.querySelector('.ciclos');
 
-let minuto = 25;
-let segundo = 60;
+let minuto = 1;
+let segundo = 0;
 let relogio;
 
 function iniciarRelogio(){
   relogio = setInterval(() => {
     timer();
+    stopCiclos();
   }, 1000);
 }
 
@@ -16,19 +18,32 @@ btn.addEventListener('click', iniciarRelogio);
 
 function timer() {
   segundo = segundo - 1;
-  minutos.innerText = minuto - 1;
 
-  if(segundo === 0) {
-    minuto = minuto - 1;
-    minutos.innerText = minuto;
-  }
   if(segundo < 0) {
     segundo = 59;
+    minuto = minuto - 1;
   }
-  
+
+  if(minuto >= 10){
+    minutos.innerText = minuto;
+  } else {
+    minutos.innerText = '0' + minuto;
+  }
+
   if(segundo < 10) {
     segundos.innerText = '0' + segundo;
   } else {
     segundos.innerText = segundo;
+  }
+}
+
+let ciclo = 1;
+
+function stopCiclos() {
+  if(minuto === 0 && segundo === 0) {
+    clearInterval(relogio);
+
+    ciclo++;
+    ciclos.innerText = '#' + ciclo;
   }
 }
